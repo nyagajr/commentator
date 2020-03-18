322a1301635ba6f7a26b55cf45fa37bf6a1d7c62
Benson Nyaga <Benson.Nyaga@jubileekenya.com>
2020-03-18 14:27:06 +0300

    displaying my model as a blade template

--- resources/views/comments/comments.blade.php
+++ resources/views/comments/comments.blade.php
@@ -2,5 +2,32 @@
 
 
 @section('title','comments')
+<h3>Comments section</h3>
+@section('content')
 
-@section('content','test')
+<ul>
+
+  <form action="#" method="">
+    <div class="">
+      <input type="text" name="name">
+      <button name=""> Add Name </button>
+    </div>
+
+  </form>
+
+  <form action="#" method="">
+    <div class="">
+      <input type="text" name="name">
+      <button name=""> Add Comment </button>
+    </div>
+
+  </form>
+@forelse( $tests as $test )
+    <li>{{ $test->name }} {{ $test->comment }}</li>
+
+  @empty
+  <li>No comments here</li>
+  @endforelse
+</ul>
+
+@endsection
